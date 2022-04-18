ee98d47e256b1385765521f7c69321c497ee983a
Evelyn Huang <evelyn@Evelyns-MacBook-Pro.local>
2022-04-18 23:28:47 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [advice, setAdvice] = useState("");
+
+  function handleClick() {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => setAdvice(data.slip))
+      .catch((error) => console.log(error));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="quote-container">
+        <h3 className="quote-number">ADVICE #{advice.id}</h3>
+        <p className="quote-text">{advice.advice}</p>
+        <img className="divider" src="/images/divider.svg" alt="divider" />
+        <br />
+        <button className="dice-wrapper rounded-circle" onClick={handleClick}>
+          <img className="dice-img" src="/images/dice.svg" alt="dice" />
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,10 @@
+const data = [
+  {
+    slip: {
+      id: 156,
+      advice: "Life is short enough, don't race to the finish.",
+    },
+  },
+];
+
+export default data;
